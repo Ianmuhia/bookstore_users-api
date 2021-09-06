f9deb78c14f2d29fe4c098854a1ad82e5c68bb38
ian <muhia>
2021-09-06 21:58:09 +0300

    created mysql connection for users database

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -1,1 +1,28 @@
 package users_db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+var (
+	UsersDB *sql.DB
+)
+
+func init() {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "*#*Johnte2536","127.0.0.1","users_db"
+	)
+
+	var err error
+	UsersDB, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+
+if err  = UsersDB.Ping();err != nil {
+	panic(err)
+}
+	log.Println("database successfully configured")
+
+}
