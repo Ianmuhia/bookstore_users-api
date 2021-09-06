e54b115a3a4919d8e0b591b9e0ee5a9fe0bd9c6c
ian <muhia>
2021-09-06 22:02:44 +0300

    imported the go mysql driver

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -4,25 +4,26 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
-	UsersDB *sql.DB
+	Client *sql.DB
 )
 
 func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "*#*Johnte2536","127.0.0.1","users_db"
-	)
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "*#*Johnte2536", "127.0.0.1", "users_db")
 
 	var err error
-	UsersDB, err = sql.Open("mysql", dataSourceName)
+	Client, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
 
-if err  = UsersDB.Ping();err != nil {
-	panic(err)
-}
+	if err = Client.Ping(); err != nil {
+		panic(err)
+	}
 	log.Println("database successfully configured")
 
 }
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"bookstore_users-api/datasources/mysql/users_db"
 	"bookstore_users-api/utils/date_utils"
 	"bookstore_users-api/utils/errors"
 	"fmt"
@@ -11,6 +12,9 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := userDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
