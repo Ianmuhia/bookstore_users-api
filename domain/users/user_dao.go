51e3a7b4759ee622adf194f773f16c2713072fae
ian <muhia>
2021-09-07 23:19:52 +0300

    find user by status dao

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -6,14 +6,16 @@ import (
 	"bookstore_users-api/utils/errors"
 	"bookstore_users-api/utils/mysql_utils"
 	"database/sql"
+	"fmt"
 )
 
 const (
-	errorNoRows     = "no rows in result set"
-	queryInsertUser = "INSERT INTO users(first_name, last_name,email,date_created) VALUES (?,?,?,?);"
-	queryGetUser    = "SELECT  id, first_name ,last_name,email,date_created FROM users WHERE id=?;"
-	queryUpdateUser = "UPDATE users SET first_name =?, last_name=?,email=? WHERE id=?"
-	queryDeleteUser = "DELETE FROM users WHERE id=?"
+	errorNoRows           = "no rows in result set"
+	queryInsertUser       = "INSERT INTO users(first_name, last_name,email,date_created) VALUES (?,?,?,?);"
+	queryGetUser          = "SELECT  id, first_name ,last_name,email,date_created FROM users WHERE id=?;"
+	queryUpdateUser       = "UPDATE users SET first_name =?, last_name=?,email=? WHERE id=?"
+	queryDeleteUser       = "DELETE FROM users WHERE id=?"
+	queryFindUserByStatus = "SELECT id, first_name, last_name,email,date_created,status FROM users WHERE status=?"
 )
 
 func (user *User) Get() *errors.RestErr {
@@ -104,4 +106,42 @@ func (user *User) Delete() *errors.RestErr {
 	}
 	return nil
 
+}
+
+func (user *User) FindByStatus(status string) ([]User, *errors.RestErr) {
+
+	stmt, err := users_db.Client.Prepare(queryFindUserByStatus)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+
+	}
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+
+		}
+	}(stmt)
+	rows, err := stmt.Query(status)
+	if err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+	results := make([]User, 0)
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated, &user.Status); err != nil {
+			return nil, mysql_utils.ParseError(err)
+		}
+		results = append(results, user)
+	}
+	if len(results) == 0 {
+		return nil, errors.NewNotFoundError(fmt.Sprintf("no users matching status %s", status))
+	}
+	return results, nil
 }
