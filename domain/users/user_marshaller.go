1761e8e01a5c8343690fd27ea6dde0fe18321ea1
ian <muhia>
2021-09-08 01:14:09 +0300

    marshaller for the public and private user reaponces

---
+++ domain/users/user_marshaller.go
@@ -0,0 +1,17 @@
+package users
+
+type PublicUser struct {
+	Id          int64  `json:"id"`
+	DateCreated string `json:"date_created"`
+	Status      string `json:"status"`
+}
+
+type PrivateUser struct {
+	Id          int64  `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+	Status      string `json:"status"`
+	//Password    string `json:"password"`
+}
