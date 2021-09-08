92c5970a3d317b47fc1ff2bcc70284bbe73c8d33
ian <muhia>
2021-09-08 03:04:26 +0300

    more configurations for the logger

--- app/application.go
+++ app/application.go
@@ -1,15 +1,20 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"bookstore_users-api/logger"
+
+	"github.com/gin-gonic/gin"
+)
 
 var (
 	router = gin.Default()
+)
 
-	)
 func StartApplication() {
 	mapUrls()
+	logger.Log.Info("about to start application")
 	err := router.Run("127.0.0.1:8090")
 	if err != nil {
-		return 
+		return
 	}
 }
--- logger/logger.go
+++ logger/logger.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	Log *zap.Logger
+	log *zap.Logger
 )
 
 func init() {
@@ -19,14 +19,34 @@ func init() {
 			MessageKey:     "msg",
 			LevelKey:       "level",
 			TimeKey:        "time",
-			EncodeLevel:    zapcore.LowercaseColorLevelEncoder,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
 			EncodeTime:     zapcore.ISO8601TimeEncoder,
 			EncodeDuration: nil,
 			EncodeCaller:   zapcore.ShortCallerEncoder,
 		},
 	}
 	var err error
-	if Log, err = logConfig.Build(); err != nil {
+	if log, err = logConfig.Build(); err != nil {
 		panic(err)
+	}
+}
+
+func GetLogger() *zap.Logger {
+	return log
+}
+
+func Info(msg string, tags ...zap.Field) {
+	log.Info(msg, tags...)
+	err := log.Sync()
+	if err != nil {
+		return
+	}
+}
+
+func Error(msg string, tags ...zap.Field) {
+	log.Error(msg, tags...)
+	err := log.Sync()
+	if err != nil {
+		return
 	}
 }
